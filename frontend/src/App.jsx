f1cca49d95408d7ad7be76e1bb5844f0ec82257a
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2025-11-05 20:56:09 +0545

    All the routes are listed in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
-
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-
+    <div className="px-4 sm:px[5vw] md:px-[7vw] lg:px-[10vw]">
+      <Navbar/> 
+      {/* Navbar will be seen in all the pages becoause we have mounted it in all the pages... */}
       
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product/:productId ' element={<Product/>}/>
+        <Route path='/contact' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+        <Route path='/orders' element={<Orders/>}/>
 
+      </Routes>
 
     </div>
   )
